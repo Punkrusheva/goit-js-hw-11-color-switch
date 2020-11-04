a5540ffa4af3aee669b9d7fd59a895c94d68a330
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2020-11-04 22:22:45 +0200

    new version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './styles.css';
 
+let timerId = null;
+
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -9,11 +11,36 @@ const colors = [
   '#795548',
 ];
 
-console.log(colors);
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
 
-const start = document.querySelector("button.start");
-console.log(start);
+startBtn.addEventListener("click", startButton);
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+stopBtn.addEventListener("click", () => {
+startBtn.classList.remove('active');
+    console.log('stop');
+    //console.log(timerId);
+    clearInterval(timerId);
+});
+
+function startButton() {
+    if (startBtn.classList.contains('active')) {
+        console.log('кнопка не активна');
+    } else {
+        startBtn.classList.add('active');
+        console.log('start');
+        timerId = setInterval(randomBackground
+        , 1000);
+    };
+    
+};
+
+function randomBackground() {
+    const min = 0;
+    const max = colors.length - 1;
+    const randomIntegerFromInterval = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    
+    document.body.style.backgroundColor = colors[randomIntegerFromInterval(min, max)];
 };
